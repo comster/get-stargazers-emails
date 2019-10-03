f38edb05174ff536c38fe8e5452626026458bf73
Jeff Pelton <j@jeffpelton.com>
2019-10-03 06:00:10 +0000

    include progress bar, remove location which can have commas (bad for csv), add dotenv for env var

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 /**
  * Module dependencies.
  */
+var dotenv = require('dotenv');
+dotenv.config();
 
 var repo = process.argv[2] || 'segmentio/analytics.js';
 var output = process.argv[3] || 'output.csv';
 var github = require('./lib/github');
 var co = require('co');
 var fs = require('fs');
+var ProgressBar = require('progress');
 
 /**
  * Do stuff.
@@ -17,11 +20,20 @@ co(function *(){
   console.log('Fetching stargazers for repo: '+repo);
   var stargazers = yield github.getStargazers(repo);
   console.log('Fetching user profiles for '+stargazers.length+' stargazers');
+
+  var bar = new ProgressBar('Profiles [:bar] :percent :etas', {
+    complete: '=',
+    incomplete: ' ',
+    width: 60,
+    total: stargazers.length
+  });
+
   for (var i = 0; i < stargazers.length; i++) {
     try {
       var user = yield github.getEmail(stargazers[i]);
-      var line = [user.username, user.name, user.location, user.email, user.hireable].join(',') + '\n';
+      var line = [user.username, user.name, user.email].join(',') + '\n';
       fs.appendFileSync(output, line, { encoding: 'utf8' });
+      bar.tick();
     } catch (e) {
       console.error(e);
     }
