15a1f68607521adc527fde370507e9d6660e2c87
Jeff Pelton <j@jeffpelton.com>
2019-10-03 05:15:47 +0000

    include github user profile details, name and location

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ co(function *(){
   for (var i = 0; i < stargazers.length; i++) {
     try {
       var user = yield github.getEmail(stargazers[i]);
-      var line = [user.username, user.email, user.hireable].join(',') + '\n';
+      var line = [user.username, user.name, user.location, user.email, user.hireable].join(',') + '\n';
       fs.appendFileSync(output, line, { encoding: 'utf8' });
     } catch (e) {
       console.error(e);
